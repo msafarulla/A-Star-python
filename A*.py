e53227bd38a920478ece3c5370f540c2b2aa649e
Mohamed <msafarulla@users.noreply.github.com>
2017-12-27 15:26:44 -0500

    Update A*.py
    My first git repository. Please excuse flaws and lack of adherence to standards.

--- A*.py
+++ A*.py
@@ -1,3 +1,26 @@
+"""
+  https://codereview.stackexchange.com/questions/181698/new-way-of-doing-the-a-algorithm
+  I am still working on this to optimizd as suggested and I will post updated code once I am done
+ 
+  note : My first git repository. Please excuse flaws and lack of adherence to standards.
+  I will get there eventually.
+ """
+
+import networkx as nx
+import pickle
+
+class Map:
+    def __init__(self, G):
+        self._graph = G
+        self.intersections = nx.get_node_attributes(G, "pos")
+        self.roads = [list(G[node]) for node in G.nodes()]
+
+def load_map(name):
+	with open(name, 'rb') as f:
+		G = pickle.load(f)
+	return Map(G)
+
+        
 def shortest_path(M, start, goal):
     explored = set([start])
     frontier = dict([(i,[start]) for i in M.roads[start]]) if start!=goal else {start:[]}
@@ -16,3 +39,11 @@ def g_h(frontier,goal,M):
 def hueristic(p1,p2,M):#use a reference point for better h, reference is vertex 0
     vt = M.intersections
     return ((vt[p1][0]-vt[0][0])**2+(vt[p1][1]-vt[0][1])**2)-((vt[p2][0]-vt[0][0])**2+(vt[p2][1]-vt[0][1])**2)
+
+
+if __name__ == '__main__':
+    maps = load_map('map.pickle')
+    starts = ?? #start node
+    goal = ?? #goal node.
+    print(shortest_path(maps, start, goal))
+    
